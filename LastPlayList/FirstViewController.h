6cb76d8522045ef257a3d9cc50e30ae517179873
Index L Prohibitorum <user@Mac-user.local>
2012-02-26 12:36:29 +0600

    Initial Commit

---
+++ LastPlayList/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  LastPlayList
+//
+//  Created by user on 26.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ LastPlayList/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  LastPlayList
+//
+//  Created by user on 26.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
